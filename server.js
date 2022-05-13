15d4173909cf52696ff4968b8c3d995999ae3afe
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-13 14:14:16 +0200

    🐛 🚀 better deployment method

--- server.js
+++
@@ -1,7 +0,0 @@
-// This is the web server, used to distribute the files in ./dist
-const express = require('express');
-const { join } = require('path');
-let app = express();
-app.use(express.static(join(__dirname, 'dist')));
-const port = process.env.PORT || 5000;
-app.listen(port);
