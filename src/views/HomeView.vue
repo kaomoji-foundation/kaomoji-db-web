de3c28e197492b8855cbe8b01107a32c7520fb6a
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-13 14:21:53 +0200

    🚀 ⚡️ Speedier deployment

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,12 @@
 <!-- import TheWelcome from '@/components/TheWelcome.vue' -->
 
 <template>
+  <div class="wrapper">
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </div>
   <main>
     <h1>Coming soon</h1>
   </main>
