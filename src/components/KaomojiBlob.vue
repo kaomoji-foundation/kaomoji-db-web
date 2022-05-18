14404846adf7c52bde623f72a705f3c1444ddef7
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-19 01:14:10 +0200

    ⚡️ copy kaomoji on click

--- src/components/KaomojiBlob.vue
+++ src/components/KaomojiBlob.vue
@@ -1,29 +1,37 @@
 <script setup lang="ts">
 import type { Kaomoji } from '@/types/kaomoji';
 
-defineProps<{
-  kaomoji: Kaomoji
+const props = defineProps<{
+    kaomoji: Kaomoji
 }>()
+
+const canCopy = !!navigator.clipboard
+
+async function copy() {
+    await navigator.clipboard.writeText(props.kaomoji.string)
+    console.info(`[copied]: ${props.kaomoji.string}`)
+}
 </script>
 
 <template>
-  <div class="mx-2 my-1 flex flex-col flex-grow items-center justify-items-start
-   min-w-max min-h-[8.5em] w-fit h-fit bg-gray-900 rounded-2xl font-NottoSansJP text-slate-100">
-    <span class="min-h-[2em] max-h-fit min-w-max mb-3 mt-6 mx-6 px-4 text-[0.6rem]
+    <div class="mx-2 my-1 flex flex-col flex-grow items-center justify-items-start
+   min-w-max min-h-[8.5em] w-fit h-fit bg-gray-900 rounded-2xl font-NottoSansJP text-slate-100"
+         v-on:click="copy()">
+        <span class="min-h-[2em] max-h-fit min-w-max mb-3 mt-6 mx-6 px-4 text-[0.6rem]
      lg:text-lg xl:text-xl font-NottoSansJP">
-      {{ kaomoji.string }}
-    </span>
-    <div class="w-[90%] h-1 bg-gray-500 rounded-full">
-      <!-- separator -->
-    </div>
-    <div class="w-[95%] h-fit m-2 flex flex-row flex-wrap items-start justify-items-start">
-      <!-- All kaomoji keys go here -->
-      <span class="bg-teal-800 text-slate-200 text-sm px-2 py-[0.025rem] mx-0.5 my-1 h-fit
+            {{ kaomoji.string }}
+        </span>
+        <div class="w-[90%] h-1 bg-gray-500 rounded-full">
+            <!-- separator -->
+        </div>
+        <div class="w-[95%] h-fit m-2 flex flex-row flex-wrap items-start justify-items-start">
+            <!-- All kaomoji keys go here -->
+            <span class="bg-teal-800 text-slate-200 text-sm px-2 py-[0.025rem] mx-0.5 my-1 h-fit
        rounded-full"
-            v-for="cat in kaomoji.categories"
-            :key="cat">
-        {{ cat }}
-      </span>
+                  v-for="cat in kaomoji.categories"
+                  :key="cat">
+                {{ cat }}
+            </span>
+        </div>
     </div>
-  </div>
 </template>
--- src/components/viewPorts/KaomojisExplorer.vue
+++ src/components/viewPorts/KaomojisExplorer.vue
@@ -2,12 +2,12 @@
 import type { Kaomoji } from "@/types/kaomoji";
 import KaomojiBlob from "../KaomojiBlob.vue";
 import { kaomojiStore } from '@/stores/kaomojis';
-import { reactive, ref } from "vue";
+import { reactive } from "vue";
 import InfiniteScrollLoader from "../InfiniteScrollLoader.vue";
 
 const props = defineProps<{
-        scope: string
-    }>()
+    scope: string
+}>()
 
 let kaoStore = kaomojiStore()
 
@@ -27,20 +27,26 @@ if (props.scope == "home") {
 </script> 
 <template>
     <main class="w-auto p-0 m-0 flex flex-col min-h-screen">
-        <div id="kaomoji-viewport" class="w-auto p-0 m-0 flex justify-center items-start">
-            <!-- tried to go using grid, tho didnt figure out the way to make it work, I think for what I want, it cant be grid, but definitely not flexbox -->
+        <div id="kaomoji-viewport"
+             class="w-auto p-0 m-0 flex justify-center items-start">
+            <!-- tried to go using grid, tho didnt figure out the way to make it work,
+             I think for what I want, it cant be grid, but definitely not flexbox -->
 
             <!-- grid gap-2 grid-cols-auto-! auto-cols-max grid-flow-row -->
             <!-- flex flex-row flex-grow items-start justify-start flex-wrap -->
             <ul class="
             min-h-fit w-full mt-4 mr-3
             flex flex-row flex-grow
-            items-start justify-start flex-wrap">      
-                <KaomojiBlob v-for="kao in kaomojis" :key="kao.id" :kaomoji="kao" >
+            items-start justify-start flex-wrap">
+                <KaomojiBlob v-for="kao in kaomojis"
+                             :key="kao.id"
+                             :id="kao.id"
+                             :kaomoji="kao">
                 </KaomojiBlob>
             </ul>
         </div>
-        <InfiniteScrollLoader :loader-fn="fillPage" scroll-element-selector="#kaomoji-viewport">
+        <InfiniteScrollLoader :loader-fn="fillPage"
+                              scroll-element-selector="#kaomoji-viewport">
         </InfiniteScrollLoader>
     </main>
 </template>
