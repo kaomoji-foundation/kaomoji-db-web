1bf53a2fbf626f6525b81497778469d1a5838c61
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-19 21:18:34 +0200

    💄 improved kaomoji style

--- src/components/KaomojiBlob.vue
+++ src/components/KaomojiBlob.vue
@@ -21,24 +21,29 @@ async function copy() {
 </script>
 
 <template>
-    <div class="mx-2 my-1 flex flex-col flex-grow items-center justify-items-start
-   min-w-max min-h-[8.5em] w-fit h-fit bg-gray-900 rounded-2xl font-NottoSansJP text-slate-100"
-         v-on:click="copy()">
-        <span class="min-h-[2em] max-h-fit min-w-max mb-3 mt-6 mx-6 px-4 text-[0.6rem]
-     lg:text-lg xl:text-xl font-NottoSansJP">
+    <a class="before:h-16 before:-mt-16 before:invisible before:block
+        relative mx-2 my-1 px-3 flex flex-col flex-grow items-center justify-around
+   min-w-max min-h-[8.5em] w-fit h-fit bg-gray-900 rounded-2xl font-NottoSansJP text-slate-100
+   border-2 hover:border-teal-800 focus:border-teal-800 border-transparent"
+       v-on:click="copy()">
+        <span class="min-h-[2em] max-h-fit min-w-max mb-3 mt-6 mx-6 px-4 text-sm
+     md:text-lg xl:text-xl font-NottoSansJP whitespace-nowrap text-clip overflow-scroll">
             {{ kaomoji.string }}
         </span>
-        <div class="w-[90%] h-1 bg-gray-500 rounded-full">
-            <!-- separator -->
+        <div class="flex flex-col items-center w-full">
+            <div class="w-full mx-7 h-1 my-0 bg-gray-500 rounded-full">
+                <!-- separator -->
+            </div>
+
+            <div class="w-full h-fit mx-2 my-1 flex flex-row flex-wrap items-start justify-start">
+                <!-- All kaomoji keys go here -->
+                <span class="bg-teal-800 text-slate-200 text-xs md:text-sm 
+            px-2 py-[0.025rem] mx-0.5 my-1 h-fit rounded-full"
+                      v-for="cat in kaomoji.categories"
+                      :key="cat">
+                    {{ cat }}
+                </span>
+            </div>
         </div>
-        <div class="w-[95%] h-fit m-2 flex flex-row flex-wrap items-start justify-items-start">
-            <!-- All kaomoji keys go here -->
-            <span class="bg-teal-800 text-slate-200 text-sm px-2 py-[0.025rem] mx-0.5 my-1 h-fit
-       rounded-full"
-                  v-for="cat in kaomoji.categories"
-                  :key="cat">
-                {{ cat }}
-            </span>
-        </div>
-    </div>
+    </a>
 </template>
