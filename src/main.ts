06fcd13e1f291f7ea6391a9d6c4f6c66c52660f2
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-24 17:55:32 +0200

    ✨ service worker w/ default config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+import "./RegisterServiceWorker";
 
 const app = createApp(App);
 
