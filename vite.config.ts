2c4aa82bc50d93c2b5fe5f6ec756f1b6e29b8b78
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-24 22:03:11 +0200

    🚧 ➕ added vite plugin for pwa

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,16 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), VitePWA({})],
+    plugins: [
+        vue(),
+        VitePWA({
+            injectRegister: "auto",
+            workbox: {
+                sourcemap: true,
+                swDest: "dist/service-worker.js",
+            },
+        }),
+    ],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
