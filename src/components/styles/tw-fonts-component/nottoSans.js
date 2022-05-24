0c81cdf15b16f49cb218851a8a38b6f335ce8f36
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2022-05-24 15:48:00 +0200

    🐛 fonts were not propertly linked when deployed

--- src/components/styles/tw-fonts-component/nottoSans.js
+++ src/components/styles/tw-fonts-component/nottoSans.js
@@ -4,25 +4,25 @@ module.exports = {
             fontFamily: 'NotoSans',
             fontStyle: 'normal',
             fontWeight: 'normal',
-            src: 'url("./fonts/NotoSans/NotoSans-Regular.ttf")'
+            src: 'url("../fonts/NotoSans/NotoSans-Regular.ttf")'
         },
         {
             fontFamily: 'NotoSans',
             fontStyle: 'italic',
             fontWeight: 'normal',
-            src: 'url("./fonts/NotoSans/NotoSans-Italic.ttf")'
+            src: 'url("../fonts/NotoSans/NotoSans-Italic.ttf")'
         },
         {
             fontFamily: 'NotoSans',
             fontStyle: 'italic',
             fontWeight: 'bold',
-            src: 'url("./fonts/NotoSans/NotoSans-BoldItalic.ttf")'
+            src: 'url("../fonts/NotoSans/NotoSans-BoldItalic.ttf")'
         },
         {
             fontFamily: 'NotoSans',
             fontStyle: 'normal',
             fontWeight: 'bold',
-            src: 'url("./fonts/NotoSans/NotoSans-Bold.ttf")'
+            src: 'url("../fonts/NotoSans/NotoSans-Bold.ttf")'
         }
     ]
 }
--- src/components/styles/tw-fonts-component/nottoSansJP.js
+++ src/components/styles/tw-fonts-component/nottoSansJP.js
@@ -4,37 +4,37 @@ module.exports = {
             fontFamily: 'NotoSansJapanese',
             fontStyle: 'normal',
             fontWeight: 'normal',
-            src: 'url("./fonts/NotoSansJP/NotoSansJP-Regular.otf")'
+            src: 'url("../fonts/NotoSansJP/NotoSansJP-Regular.otf")'
         },
         {
             fontFamily: 'NotoSansJapanese',
             fontStyle: 'normal',
             fontWeight: 'thin',
-            src: 'url("./fonts/NotoSansJP/NotoSansJP-Thin.otf")'
+            src: 'url("../fonts/NotoSansJP/NotoSansJP-Thin.otf")'
         },
         {
             fontFamily: 'NotoSansJapanese',
             fontStyle: 'normal',
             fontWeight: 'medium',
-            src: 'url("./fonts/NotoSansJP/NotoSansJP-Medium.otf")'
+            src: 'url("../fonts/NotoSansJP/NotoSansJP-Medium.otf")'
         },
         {
             fontFamily: 'NotoSansJapanese',
             fontStyle: 'normal',
             fontWeight: 'lignt',
-            src: 'url("./fonts/NotoSansJP/NotoSansJP-Light.otf")'
+            src: 'url("../fonts/NotoSansJP/NotoSansJP-Light.otf")'
         },
         {
             fontFamily: 'NotoSansJapanese',
             fontStyle: 'normal',
             fontWeight: 'bold',
-            src: 'url("./fonts/NotoSansJP/NotoSansJP-Bold.otf")'
+            src: 'url("../fonts/NotoSansJP/NotoSansJP-Bold.otf")'
         },
         {
             fontFamily: 'NotoSansJapanese',
             fontStyle: 'normal',
             fontWeight: 'medium',
-            src: 'url("./fonts/NotoSansJP/NotoSansJP-Black.otf")'
+            src: 'url("../fonts/NotoSansJP/NotoSansJP-Black.otf")'
         },
     ],
 }
