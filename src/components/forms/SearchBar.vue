30fc136babb6bc677d35725b3c209d4d41276c94
zark0 <58535242+zark0-UwU@users.noreply.github.com>
2023-06-13 16:22:01 +0200

    🚸 scroll top on search value change

--- src/components/forms/SearchBar.vue
+++ src/components/forms/SearchBar.vue
@@ -19,6 +19,13 @@ function focusSearchBar() {
     }
 }
 
+function focusTop() {
+    setTimeout(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+    }, 200
+    )
+}
+
 </script>
 
 <template>
@@ -26,31 +33,29 @@ function focusSearchBar() {
          class="flex justify-center w-1/2 bg-gray-800">
         <form method="dialog"
               class="w-full">
-            <div class="relative text-gray-600 focus-within:text-gray-400
-                transition-colors duration-300">
+            <div class="relative text-gray-600 transition-colors duration-300 focus-within:text-gray-400">
                 <input type="text"
                        name="q"
                        placeholder="Search..."
                        autocomplete="on"
                        v-model.trim="filters.queryString"
+                       v-on:input="focusTop()"
                        ref="searchBar"
-                       class="py-2 text-sm w-full text-white bg-gray-800 rounded-md pl-10 
-                       focus:outline-none focus:bg-gray-900 focus:text-gray-100 
-                       transition-colors duration-300" />
-                <span class="absolute inset-y-0 right-0 flex items-center pl-2">
+                       class="py-2 pl-10 w-full text-sm text-white bg-gray-800 rounded-md transition-colors duration-300 focus:outline-none focus:bg-gray-900 focus:text-gray-100" />
+                <span class="flex absolute inset-y-0 right-0 items-center pl-2">
                     <button type="button"
                             v-if="!filters.queryString"
                             v-on:click="focusSearchBar()"
                             class="p-1 my-v-center focus:outline-none focus:shadow-outline">
                         <ion-icon name="search"
-                                  class="text-slate-50 h-6 w-6"></ion-icon>
+                                  class="w-6 h-6 text-slate-50"></ion-icon>
                     </button>
                     <button type="button"
                             v-if="filters.queryString"
                             v-on:click="filters.queryString = ''"
                             class="p-1 my-v-center focus:outline-none focus:shadow-outline">
                         <ion-icon name="close"
-                                  class="text-slate-50 h-6 w-6"></ion-icon>
+                                  class="w-6 h-6 text-slate-50"></ion-icon>
                     </button>
                 </span>
             </div>
